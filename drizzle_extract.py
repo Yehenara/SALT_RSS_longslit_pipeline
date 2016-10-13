e22c4a60f4d810d8d86c4a7d4c7e13fc78d54f16
Ralf Kotulla <ralf.kotulla@gmail.com>
2016-10-13 14:46:57 -0500

    added rectification routine and new standalone rectify tasks that re-drizzles the frame onto the new, user-defined wavelength grid.

--- drizzle_extract.py
+++ drizzle_extract.py
@@ -9,6 +9,93 @@ from optparse import OptionParser
 import pysalt.mp_logging
 import logging
 
+
+
+def drizzle_spec(img_data, wl, var_data, out_wl):
+
+    if (img_data.ndim == 1):
+        img_data = img_data.reshape((-1,1))
+    if (wl.ndim == 1):
+        wl = wl.reshape((-1,1))
+    if (var_data.ndim == 1):
+        var_data = var_data.reshape((-1,1))
+
+    #
+    # prepare the output flux array, and initialize with NaNs
+    #
+    out_flux = numpy.zeros_like(out_wl)
+    out_flux[:] = numpy.NaN
+    out_var = numpy.zeros_like(out_wl)
+    out_var[:] = numpy.NaN
+    #print out_wl
+
+    #
+    # prepare a padded WL array
+    #
+    wl_data_padded = numpy.pad(wl, ((0, 0), (1, 1)), mode='edge') #linear_ramp')
+    #print wl_data.shape, wl_data_padded.shape
+    wl_width = 0.5*(wl_data_padded[:, 2:] - wl_data_padded[:, :-2])
+    wl_from = 0.5*(wl_data_padded[:, 0:-2] + wl_data_padded[:,1:-1])
+    wl_to = 0.5*(wl_data_padded[:, 1:-1] + wl_data_padded[:,2:])
+    #print wl_width.shape, wl_from.shape, wl_to.shape
+
+    # compute the flux density array (i.e. flux per wavelength) by dividing the
+    # flux image (which is flux integrated over the width of a pixel) by the width of each pixel
+    img_data_per_wl = img_data / wl_width
+    var_data_per_wl = var_data / wl_width
+
+
+    #
+    # now drizzle the data into the output container
+    #
+    wl_width_1d = wl_width.ravel()
+    wl_from_1d = wl_from.ravel()
+    wl_to_1d = wl_to.ravel()
+    wl_data_1d = wl.ravel()
+    img_data_per_wl_1d = img_data_per_wl.ravel()
+    var_data_1d = var_data.ravel()
+    var_data_per_wl_1d = var_data_per_wl.ravel()
+    wl_width_1d = wl_width.ravel()
+
+    wl0 = out_wl[0]
+    dwl = out_wl[1] - out_wl[0]
+
+    for px in range(wl_width_1d.shape[0]):
+
+        # find first and last pixel in output array to receive some of the flux of this input pixel
+        first_px = (wl_from_1d[px] - wl0) / dwl
+        last_px = (wl_to_1d[px] - wl0) / dwl
+
+        pixels = range(int(math.floor(first_px)), int(math.ceil(last_px)))  # , dtype=numpy.int)
+        # print wl_data_1d[px], wl_width_1d[px], first_px, last_px, pixels
+        for i, tp in enumerate(pixels):
+
+            if (i == 0 and i == len(pixels) - 1):
+                # all flux in a single output pixel
+                fraction = last_px - first_px
+            elif (i == 0):
+                # first pixel, with more to come
+                fraction = (tp + 1.) - first_px
+            elif (i == len(pixels) - 1):
+                # last of many pixels
+                fraction = last_px - tp
+            else:
+                # some pixel in the middle
+                fraction = 1.
+
+            if (numpy.isnan(out_flux[tp])):
+                out_flux[tp] = 0.
+                out_var[tp] = 0.
+            out_flux[tp] += fraction * img_data_per_wl_1d[px]  # * (dwl / wl_width_1d[px])
+            out_var[tp] += fraction * var_data_per_wl_1d[px]  # * (dwl / wl_width_1d[px])
+            # print "     ", tp, fraction
+
+    return out_flux, out_var
+
+
+
+
+
 def drizzle_extract_spectrum(fits_fn, out_fn,
                              input_ext="SKYSUB.OPT",
                              minwl=-1, maxwl=-1, dwl=-1,
---
+++ rectify.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+import astropy.io.fits as fits
+import os
+import sys
+import numpy
+from optparse import OptionParser
+import logging
+
+import drizzle_extract
+
+
+
+if __name__ == "__main__":
+
+    input_file = sys.argv[1]
+    if (not os.path.isfile(input_file)):
+        print "Input file does not exist"
+        sys.exit(0)
+
+    parser = OptionParser()
+    parser.add_option("", "--input", dest="inputext",
+                      help="name of extension to extract",
+                      default="SKYSUB.OPT")
+    parser.add_option("-n", "--minwl", dest="minwl",
+                      help="minimum wavelength of output",
+                      default=-1, type=float)
+    parser.add_option("-x", "--maxwl", dest="maxwl",
+                      help="maximum wavelength of output",
+                      default=-1, type=float)
+    parser.add_option("-d", "--dwl", dest="dwl",
+                      help="wavelength sampling / dispersion",
+                      default=-1, type=float)
+    parser.add_option("-f", "--format", dest="format",
+                      help="output format (FITS/ascii)",
+                      default="fits", type=str)
+    (options, cmdline_args) = parser.parse_args()
+
+
+    hdulist = fits.open(input_file)
+
+    img_data = hdulist['SKYSUB.OPT'].data
+    wl_data = hdulist['WAVELENGTH'].data
+    var_data = hdulist['VAR'].data
+
+    wl0 = options.minwl
+    wlmax = options.maxwl
+    dwl = options.dwl
+    minwl = options.minwl
+
+    if (minwl < 0):
+        wl0 = numpy.min(wl_data)
+    if (wlmax < 0):
+        wlmax = numpy.max(wl_data)
+    if (dwl < 0):
+        _disp = numpy.diff(wl_data)
+        dwl = 0.5*numpy.min(_disp)
+        if (dwl <= 0):
+            dwl = 0.1
+
+    # Now prepare the output array
+    logger = logging.getLogger("RectifySpec")
+    out_wl_count = int((wlmax - wl0) / dwl) + 1
+    logger.info("output: %d wavelength points from %f to %f in steps of %f angstroems" % (
+        out_wl_count, wl0, wlmax, dwl))
+    out_wl = numpy.arange(out_wl_count, dtype=numpy.float) * dwl + wl0
+
+    rect_img = numpy.empty((img_data.shape[0], out_wl.shape[0]))
+    rect_var = numpy.empty((img_data.shape[0], out_wl.shape[0]))
+    rect_img[:,:] = numpy.NaN
+    rect_var[:,:] = numpy.NaN
+
+    for y in range(0, img_data.shape[0]):
+
+        sys.stdout.write("\rRectifying line %4d of %4d" % (y+1, img_data.shape[0])); sys.stdout.flush()
+
+        line_img = img_data[y:y+1, :]
+        line_wl = wl_data[y:y+1, :]
+        line_var = var_data[y:y+1, :]
+
+        drizzled = drizzle_extract.drizzle_spec(line_img, line_wl, line_var, out_wl)
+        _data, _var = drizzled
+
+        rect_img[y, :] = _data[:]
+        rect_var[y, :] = _var[:]
+
+    print ("\nAll done, preparing output")
+    hdus = [None]*3
+    for i in range(1,3):
+        imghdu = fits.ImageHDU()
+
+        imghdu.header['WCSNAME'] = "wavelength"
+
+        imghdu.header['CRPIX1'] = 1.0
+        imghdu.header['CRPIX2'] = 1.0
+
+        imghdu.header['CRVAL1'] = wl0
+        imghdu.header['CTYPE1'] = 'AWAV'
+        imghdu.header['CUNIT1'] = "Angstrom"
+        imghdu.header['CD1_1'] = dwl
+
+        imghdu.header['CRVAL2'] = 1.
+        imghdu.header['CTYPE2'] = 'POS'
+        imghdu.header['CUNIT2'] = 'pixel'
+        imghdu.header['CD2_2'] = 1.0
+
+        hdus[i] = imghdu
+
+    hdus[1].data = rect_img
+    hdus[1].name = "SCI.RECT"
+
+    hdus[2].data = rect_var
+    hdus[2].name = 'VAR.RECT'
+
+    hdus[0] = fits.PrimaryHDU()
+
+    hdulist = fits.HDUList(hdus)
+    hdulist.writeto(sys.argv[2], clobber=True)
+    #fits.PrimaryHDU(data=rect_img).writeto("rect_img.fits", clobber=True)
+    #fits.PrimaryHDU(data=rect_var).writeto("rect_var.fits", clobber=True)
